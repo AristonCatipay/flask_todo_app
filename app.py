df1c0ce7581b6ab1a1d5c2f4b03742e9ed69afa5
Ariston <aristoncatipay123@gmail.com>
2023-12-14 17:43:12 +0800

    Fix the error in the database and in the functions where it redirects to home.

--- app.py
+++ app.py
@@ -4,17 +4,18 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configuration for the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Todo model
 class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-db.create_all()
+with app.app_context():
+    db.create_all()
 
 @app.get("/")
 def read():
@@ -27,18 +28,18 @@ def create():
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("read"))
 
 @app.get("/update/<int:todo_id>")
 def update(todo_id):
     todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("read"))
 
 @app.get("/delete/<int:todo_id>")
 def delete(todo_id):
     todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("read"))
